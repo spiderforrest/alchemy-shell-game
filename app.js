95bc9b2f13c42453018ad39557f63255a48cd5ed
spiderforrest <spider@spood.org>
2022-10-31 16:43:25 -0700

    added handling per element (this should not have taken this long)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // get the one cup that's in html
 const cupHead = document.getElementById('cup-0');
+const addCupButton = document.getElementById('add-cup');
 
 // set the number of cups to one
 let totalCups = 1;
@@ -7,15 +8,24 @@ let totalCups = 1;
 function addCup() {
     // clone the existing cup div
     const newCup = cupHead.cloneNode(true);
-    // generate unique id for the cup and increment var
+    // generate unique id for the cup
     newCup.setAttribute('id', 'cup-' + totalCups);
-    totalCups++;
     // add the cup to the list
     document.getElementById('cup-box').appendChild(newCup);
+    // add event listener to the new cup
+    newCup.addEventListener('click', () => {
+        handleGuess(newCup.getAttribute('id'));
+    });
+    totalCups++;
+}
+
+function handleGuess(guess) {
+    const ballLocation = Math.floor(Math.random() * totalCups);
+    console.log('clicked on: ' + guess);
+    console.log('right answer: ' + ballLocation);
 }
 
 // tmp testing button
-const tmpbutt = document.getElementById('tmpbutt');
-tmpbutt.addEventListener('click', () => {
+addCupButton.addEventListener('click', () => {
     addCup();
 });
